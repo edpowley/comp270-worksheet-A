bd2dcd98c0378dff7f523ae6391a17561a3dcd59
edpowley <edpowley@gmail.com>
2019-09-06 00:04:47 +0100

    Some refactoring

--- Bezier.cpp
+++ Bezier.cpp
@@ -13,12 +13,7 @@ Vector2 Bezier::getPoint(float t) const
 {
 	float s = 1 - t;
 
-	float x = s * s*s*m_points[0].getX() + 3 * s*s*t*m_points[1].getX() + 3 * s*t*t*m_points[2].getX() + t * t*t*m_points[3].getX();
-	float y = s * s*s*m_points[0].getY() + 3 * s*s*t*m_points[1].getY() + 3 * s*t*t*m_points[2].getY() + t * t*t*m_points[3].getY();
-
 	return s * s*s*m_points[0] + 3 * s*s*t*m_points[1] + 3 * s*t*t*m_points[2] + t * t*t*m_points[3];
-	
-	//return Vector2(x, y);
 }
 
 Vector2 Bezier::getTangent(float t) const
@@ -37,7 +32,7 @@ void Bezier::draw(SDL_Renderer *renderer) const
 		float t = (float)i / 20.0f;
 		Vector2 p = getPoint(t);
 
-		SDL_RenderDrawLineF(renderer, last.getX(), last.getY(), p.getX(), p.getY());
+		SDL_RenderDrawLineF(renderer, last.x, last.y, p.x, p.y);
 		last = p;
 	}
 }
--- Vector2.h
+++ Vector2.h
@@ -3,31 +3,28 @@
 class Vector2
 {
 public:
-	Vector2() : m_x(0), m_y(0) { }
-	Vector2(float x, float y) : m_x(x), m_y(y) { }
-
-	float getX() const { return m_x; }
-	float getY() const { return m_y; }
+	Vector2() : x(0), y(0) { }
+	Vector2(float x, float y) : x(x), y(y) { }
 
 	Vector2 operator+(const Vector2& other) const
 	{
-		return Vector2(m_x + other.m_x, m_y + other.m_y);
+		return Vector2(x + other.x, y + other.y);
 	}
 
 	Vector2 operator-(const Vector2& other) const
 	{
-		return Vector2(m_x - other.m_x, m_y - other.m_y);
+		return Vector2(x - other.x, y - other.y);
 	}
 
 	Vector2 operator*(float scalar) const
 	{
-		return Vector2(scalar * m_x, scalar * m_y);
+		return Vector2(scalar * x, scalar * y);
 	}
 
-	float m_x, m_y;
+	float x, y;
 };
 
 inline Vector2 operator*(float scalar, const Vector2& vector)
 {
-	return Vector2(scalar * vector.m_x, scalar * vector.m_y);
+	return Vector2(scalar * vector.x, scalar * vector.y);
 }
