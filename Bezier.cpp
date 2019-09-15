f0cefb2466318cbdd4fbb6f1d25a520d5e16d0e9
edpowley <edpowley@gmail.com>
2019-09-15 22:57:40 +0100

    Stripping out solution code

--- Bezier.cpp
+++ Bezier.cpp
@@ -6,30 +6,7 @@ Bezier::Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Ve
 {
 }
 
-Vector2 Bezier::getPoint(float t) const
-{
-	float s = 1 - t;
-
-	return s * s*s*p0 + 3 * s*s*t*p1 + 3 * s*t*t*p2 + t * t*t*p3;
-}
-
-Vector2 Bezier::getTangent(float t) const
-{
-	float s = 1 - t;
-
-	return 3 * s*s*(p1 - p0) + 6 * s*t*(p2 - p1) + 3 * t*t*(p3 - p2);
-}
-
 void Bezier::draw(SDL_Renderer *renderer) const
 {
-	Vector2 last = p0;
-
-	for (int i = 1; i <= 20; i++)
-	{
-		float t = (float)i / 20.0f;
-		Vector2 p = getPoint(t);
-
-		SDL_RenderDrawLineF(renderer, last.x, last.y, p.x, p.y);
-		last = p;
-	}
+	// TODO
 }
--- Bezier.h
+++ Bezier.h
@@ -7,9 +7,6 @@ class Bezier
 public:
 	Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3);
 
-	Vector2 getPoint(float t) const;
-	Vector2 getTangent(float t) const;
-
 	void draw(SDL_Renderer *renderer) const;
 
 private:
